ebe635a770c799606442ecabb34ccdcde8c1211a
Bogdan Tanase <bogdan.tanase@1and1.ro>
2018-08-09 18:10:37 +0300

    Added Entities First Version

--- src/classes/Map.java
+++ src/classes/Map.java
@@ -1,9 +1,37 @@
 package classes;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Map extends JPanel{
-    public Map(){
+    private Image background;
 
-    };
+
+
+    public Map(){
+        initBoard();
+    }
+
+    private void initBoard() {
+        loadBackground();
+        setPreferredSize(new Dimension(100,600));
+    }
+
+    private void loadBackground() {
+        try {
+            background = ImageIO.read(new File("src/pictures/background.png"));
+        }
+        catch(IOException ex){
+            System.out.println("Background could not be found.");
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        g.drawImage(background,0,0,null);
+    }
+
 }
